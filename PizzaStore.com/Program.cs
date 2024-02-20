8d2169372b53adf3c6c51e6f796dc95b1de30164
egebo <egebo@LASSES-BRBAR>
2024-02-20 13:20:23 +0100

    Add project files.

---
+++ PizzaStore.com/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaStore.com
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
